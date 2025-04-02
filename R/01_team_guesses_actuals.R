7274028fd6d0df08d98fffda3705bb9079ef26dc
Alana <ajclason@gmail.com>
2025-04-01 22:43:25 -0700

    code redone for 2025

--- R/01_team_guesses_actuals.R
+++ R/01_team_guesses_actuals.R
@@ -7,11 +7,25 @@ in_dir <- "data"
 schuss_out <- fread(file.path(in_dir,"Schuss Results Export Test.csv"))
 colnames(schuss_out) <- c("bib", "gender", "first", "last", "y", "div", "time",
                           "team", "team_time","x")
-
+str(schuss_out)
+schuss_out[, time_sec := as.integer(sub(":.*", "", 
+                                        time)) * 60 + as.numeric(sub(".*:", "", 
+                                                                     time))]
 #2. schuss guess time
 schuss_guess <- fread(file.path(in_dir,"Schuss guess.csv"))
+str(schuss_guess)
+schuss_guess[, guess_time_sec := as.integer(sub(":.*", "", 
+                                                guess_time)) * 60 + as.numeric(sub(".*:", "", 
+                                                                     guess_time))]
 
 #3. merge
-schuss <- merge(schuss_out, schuss_guess[,.(bib,guess_time)])
+schuss <- merge(schuss_out, schuss_guess[,.(bib,guess_time, guess_time_sec)])
 
-schuss[, guess_act := (guess_time - time), by = "bib"]
+schuss[, guess_diff := abs(guess_time_sec - time_sec), by = "bib"]
+
+schuss_teams <- schuss[, .(team_guess = sum(guess_diff, na.rm = TRUE)), 
+                       by = "team"]
+
+setkey(schuss_teams, team_guess)
+
+fwrite(schuss_teams, file.path(in_dir, "schuss_teams_out.csv"))
